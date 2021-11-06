de5f3c6f0bde1ad0ca9841853caddd9270707a51
JosephAbbey <66320873+LabyrinthLion@users.noreply.github.com>
2021-11-06 20:57:44 +0000

    minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+mod lib;
+use lib::{run, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
