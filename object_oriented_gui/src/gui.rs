ade4b1767500374fb3e3253bbc9bd68c50ca5169
JosephAbbey <66320873+LabyrinthLion@users.noreply.github.com>
2021-11-06 17:12:14 +0000

    object_oriented_gui

---
+++ object_oriented_gui/src/gui.rs
@@ -0,0 +1,30 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!(
+            "Button {}, {}, is labelled {}.",
+            self.width, self.height, self.label
+        );
+    }
+}
